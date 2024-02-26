df9a26c44f890604d69de2a511629a2bbd947932
Yeiba <yeibayalda@gmail.com>
2024-02-26 21:57:57 +0100

    jacob

--- src/App.js
+++ src/App.js
@@ -24,27 +24,27 @@ import 'aos/dist/aos.css';
 
 function App() {
 
-      return (
-          
-          <div className="App">
-            <Helmet>
-              <meta name="format-detection" content="telephone=no" />
-              <meta
-                name="description"
-                content="Quinn Studios #BRANDING #stRATEGY #consulting #graphic_DESIGN #motion_graphic
+  return (
+
+    <div className="App">
+      <Helmet>
+        <meta name="format-detection" content="telephone=no" />
+        <meta
+          name="description"
+          content="Quinn Studios #BRANDING #stRATEGY #consulting #graphic_DESIGN #motion_graphic
                 #marketing #digital_creation #developement #evants #organization #advertising  #film_making  #producing #products_design #3D_creation"
-              />
-            </Helmet>
-            <Navbar /> 
-            <Home /> 
-            <Services /> 
-            <Projects /> 
-            <About /> 
-            <Contact /> 
-          </div>
-      
-      );
-    
+        />
+      </Helmet>
+      <Navbar />
+      <Home />
+      <Services />
+      <Projects />
+      <About />
+      <Contact />
+    </div>
+
+  );
+
 }
 
 export default App;
